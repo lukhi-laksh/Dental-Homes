8628784cea2122a1cd2d8f2b46d8e53c2e08b919
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-03-27 01:34:31 +0530

    remove first section

--- aboutus.php
+++ aboutus.php
@@ -123,29 +123,6 @@
 
   <main>
     <article>
-      <section class="section hero" id="home" style="background-image: url('./assets/images/hero-bg.png')"
-        aria-label="hero">
-        <div class="container">
-
-          <div class="hero-content">
-
-            <p class="section-subtitle">About Us</p>
-
-            <h1 class="h1 hero-title">We Are Best Dental Service</h1>
-
-            <p class="hero-text">
-            Live freely, not with constrained movements. The solution lies in balance. 
-            Let the day pass with flexibility, allowing growth while maintaining a strong course, 
-            ensuring the journey is smooth and free from unnecessary burdens.
-            </p>
-          </div>
-
-          <figure class="hero-banner">
-            <img src="./assets/images/hero-banner.jpeg" width="587" height="839" alt="hero banner" class="w-100">
-          </figure>
-
-        </div>
-      </section>
       <section class="section about" id="about" aria-label="about">
         <div class="container">
 

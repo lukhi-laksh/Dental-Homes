0772eeeb340ae544bb032574c520164ab58d5730
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-04-02 19:29:41 +0530

    name to email change

--- hms/doctor/index.php
+++ hms/doctor/index.php
@@ -64,7 +64,7 @@ header("location:index.php");
 								Sign in to your account
 							</legend>
 							<p>
-								Please enter your name and password to log in.<br />
+								Please enter your email and password to log in.<br />
 								<span style="color:red;"><?php echo $_SESSION['errmsg']; ?><?php echo $_SESSION['errmsg']="";?></span>
 							</p>
 							<div class="form-group">
